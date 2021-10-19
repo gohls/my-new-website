61c8a66138d63a4090302d96d34d6d25ee7987ed
gohls <sagohl@gmail.com>
2021-10-18 23:12:29 -0400

    Basic set up work

--- server/src/mysite/urls.py
+++ server/src/mysite/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from imdb import views
 
+
+ROUTER = routers.DefaultRouter()
+ROUTER.register(r'users', views.UserViewSet)
+ROUTER.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(ROUTER.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
